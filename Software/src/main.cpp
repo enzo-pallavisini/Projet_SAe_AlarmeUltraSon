4c456df3427519b6a0e8900788fa271a5337e194
Enzo <enzo.pallavisini@etu.univ-smb.fr>
2026-05-12 14:00:59 +0200

    lire code

--- Software/src/main.cpp
+++ Software/src/main.cpp
@@ -1,12 +1,28 @@
-#include <Arduino.h>
 #include "Projet_AlarmeUltraSon.hpp"
-
-
-void setup() {
-  // put your setup code here, to run once:
   
-}
+ void setup()  
+ { 
+   //Serial initialization 
+   Serial.begin(9600);                        // Sets the baud rate to 9600 
+   pinMode(Sortie_Led,OUTPUT);
+   pinMode(URTRIG,OUTPUT);                    // A low pull on pin COMP/TRIG 
+   digitalWrite(URTRIG,HIGH);                 // Set to HIGH  
+   pinMode(URECHO, INPUT);                    // Sending Enable PWM mode comand 
+   delay(500); 
+   Serial.println("Init the sensor"); 
+  
+  } 
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+ void loop() 
+ { 
+   PWM_Mode(); 
+   delay(100);
+   Allumer_Led();
+   
+   /*tone(10,1000);
+   delay(500);
+   tone(10,10000);
+   delay(500);
+   */
+ }  
+  
